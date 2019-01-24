9c53e188e20e01b496beef483d462b417c1ba121
Craig Melville <acekreations@gmail.com>
2019-01-23 23:59:57 -0800

    basic coin flip functionality

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    componentDidMount() {
+        this.flipCoin();
+    }
+
+    flipCoin = () => {
+        var coin = document.querySelector(".coin");
+        var thisComp = this;
+        coin.addEventListener("click", function() {
+            var randNum = Math.floor(Math.random() * 11);
+            var i = 0;
+            thisComp.coinToggle(coin, randNum, i, thisComp.coinToggle);
+        });
+    };
+    coinToggle = (coin, randNum, i, cb) => {
+        coin.classList.toggle("is-flipped");
+        if (i < randNum) {
+            i++;
+            setTimeout(function() {
+                cb(coin, randNum, i, cb);
+            }, 200);
+        }
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="coin">
+                    <div className="coin__face coin__face--front" />
+                    <div className="coin__face coin__face--back" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
